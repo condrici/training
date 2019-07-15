f27df52b98a7cd76d573f04185a35f3ae5447326
What Else? <example@example.com>
2019-07-15 11:49:47 +0300

    Update comments

--- php7.php
+++ php7.php
@@ -6,6 +6,36 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
+/********************************************************************************
+ *
+ * What's new in PHP 7
+ *
+ * ******************************************************************************
+ *
+ *
+ * Scalar (argument) type declarations (int, string, float)
+ * Return type declarations
+ * Null coalescing operator
+ * Spaceship operator
+ * Constant arrays using define()
+ * Anonymous classes
+ * Unicode codepoint escape syntax
+ * Closure::call()
+ * Filtered unserialize()
+ * IntlChar
+ * Expectations
+ * Group use declarations
+ * Generator Return Expressions
+ * Generator delegation
+ * Integer division with intdiv()
+ * Session options
+ * preg_replace_callback_array()
+ * CSPRNG Functions
+ * list()
+ *
+ *
+ ********************************************************************************/
+
 /**
  * [0]
  *
